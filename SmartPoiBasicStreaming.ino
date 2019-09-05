a9b547488d4437b0a40bcc1ee7a9f7adf076ff37
tom <tomjuggler@gmail.com>
2019-09-05 19:41:59 +0200

    Added Readme and Send to Processing emulator code
    Signed-off-by: tom <tomjuggler@gmail.com>

--- SmartPoiBasicStreaming.ino
+++ SmartPoiBasicStreaming.ino
@@ -59,6 +59,19 @@ byte packetBuffer[NUM_PX]; //buffer to hold incoming packet
 
 WiFiUDP Udp;
 
+//more send to processing stuff:
+#define UPDATES_PER_SECOND 30000
+boolean emulated = true;
+//
+/****Variables needed for sending to Processing. */
+uint16_t sendDelay = 10;    // [Milliseconds] To slow stuff down if needed.
+boolean testing = false;  // Default is false. [Ok to change for testing.]
+// Can temporarily change testing to true to check output in serial monitor.
+// Must set back to false to allow Processing to connect and receive data.
+
+boolean linkedUp = true;  // Initially set linkup status false. [Do Not change.]
+/****End of variables needed for sending Processing. */
+
 void setup() {
 //  Serial.begin(115200);
   FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, BGR>(leds, NUM_LEDS); //DATA_RATE_MHZ(8)
@@ -71,7 +84,15 @@ void setup() {
   dnsServer.start(DNS_PORT, "*", apIP); //AP mode only
   Udp.begin(localPort);
   fastLEDIndicate();
-  
+  /****Stuff needed in setup() section for sending to Processing. */
+  Serial.begin(115200);  // Allows serial output.
+  if (emulated) {
+    //  while (!Serial) {
+    //    ;  // Wait for serial connection. Only needed for Leonardo board.
+    //  }
+    firstContact();  // Connect with Processing. Hello, is anyone out there?
+    /****End of stuff to put in your setup() section for Processing. */
+  }
 }
 
 void loop() {
@@ -105,9 +126,14 @@ void loop() {
 
     //FastLED.delay(2); //not just for emulator!
     LEDS.show();
-    delay(1);
+    FastLED.delay(1);
+//    delay(1);
     ///////////////////////////////////end FastLed Code//////////////
-
+if (emulated) {
+    SendToProcessing();
+//    delay(1);
+    FastLED.delay(1000 / UPDATES_PER_SECOND);
+  }
     // send a reply, to the IP address and port that sent us the packet we received
     //    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
     //    Udp.write(ReplyBuffer);
@@ -164,3 +190,57 @@ void fastLEDIndicate(){
 }
 
 
+/****The below two functions are needed for sending to Processing. */
+
+// Waits for Processing to respond and then sends the number of pixels.
+void firstContact() {
+  uint16_t nLEDS = NUM_LEDS;  // Number to send to Processing.  (Allows up to 65,535 pixels)
+  if (testing == true) {
+    linkedUp = true;  // If testing, pretend we are already connected to Processing.
+    Serial.print("NUM_LEDS: "); Serial.println(nLEDS);  // Number of pixels in our LED strip.
+    Serial.print("  High Byte = "); Serial.print(highByte(nLEDS));  // The high byte.
+    Serial.print(" x 256 = "); Serial.println(highByte(nLEDS) * 256);
+    Serial.print("  Low Byte  = "); Serial.println(lowByte(nLEDS));  // The low byte.
+    delay(3000);
+  }
+  else {
+    while (Serial.available() <= 0) {  // Repeats until Processing responds. Hello?
+      Serial.print('A');  // send an ASCII A (byte of value 65)
+      delay(100);
+    }
+    // Once Processing responds send the number of pixels as two bytes.
+    Serial.write(highByte(nLEDS));  // Send the high byte to Processing.
+    Serial.write(lowByte(nLEDS));  // Send the low byte to Processing.
+    Serial.print('#');  // send an ASCII # (byte of value 35) as a flag for Processing.
+    linkedUp = true;  // Now that Processing knows number of pixels set linkedUp to true.
+    delay(500);
+  }
+}
+
+//--------------------
+// This function sends ALL the pixel data to Processing.
+void SendToProcessing() {
+  if (testing == true) { // Print pixel data. If NUM_LEDS is large this will be a lot of data!
+    Serial.println("-------------------------------------------------------");
+    for (uint16_t d = 0; d < NUM_LEDS; d++) {
+      Serial.print("p: "); Serial.print(d);
+      Serial.print("\tr: "); Serial.print(leds[d].g);
+      Serial.print("\tg: "); Serial.print(leds[d].b);
+      Serial.print("\tb: "); Serial.println(leds[d].r);
+    }
+    Serial.println(" ");
+    delay(500);  // Add some extra delay while testing.
+  }
+  else {  // Send ALL the pixel data to Processing!
+    for (uint16_t d = 0; d < NUM_LEDS; d++) {
+      Serial.write(d);          // Pixel number
+      Serial.write(leds[d].g);  // Red channel data
+      Serial.write(leds[d].b);  // Green channel data
+      Serial.write(leds[d].r);  // Blue channel data
+    }
+    delay(sendDelay);  // Delay to slow things down if needed.
+  }
+}
+
+//--------------------
+/****End of the functions needed for sending to Processing.*/
